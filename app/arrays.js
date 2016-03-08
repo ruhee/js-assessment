4a7be8fa8c66296880cdb488a4c14e3a81330b7f
Ruhee Dewji <ruhee.dewji@invivo.com>
2016-03-07 22:22:55 -0500

    remove all instances of a value from an array

--- app/arrays.js
+++ app/arrays.js
@@ -21,7 +21,12 @@ exports.arraysAnswers = {
   },
 
   remove : function(arr, item) {
-
+    for(var i = 0; i < arr.length; i++){
+      if(arr[i] === item){
+        arr.splice(i, 1);
+      }
+    }
+    return arr;
   },
 
   removeWithoutCopy : function(arr, item) {
